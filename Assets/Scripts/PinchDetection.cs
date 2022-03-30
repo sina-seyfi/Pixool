0eda379dbe03687ea0a5443a0c61371d85ead238
Sina Seyfi <sina.seyfi.1994@gmail.com>
2022-03-30 20:29:50 +0430

    Pinch to Zoom Implemented
    Implemented pinch to zoom with new Input System of Unity

---
+++ Assets/Scripts/PinchDetection.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchDetection : MonoBehaviour
+{
+	[SerializeField]
+	private float speed = 1f;
+	[SerializeField]
+	private float minScale = 0.5f;
+	[SerializeField]
+	private float maxScale = 3.0f;
+    private TouchControls controls;
+	private Coroutine zoomCoroutine;
+	[SerializeField]
+	private GameObject go;
+	private void Awake() {
+		controls = new TouchControls();
+	}
+	private void OnDisable() {
+		controls.Disable();
+	}
+	private void OnEnable() {
+		controls.Enable();
+	}
+	private void Start() {
+		controls.Touch.SecondaryTouchContact.started += _ => ZoomStart();
+		controls.Touch.SecondaryTouchContact.canceled += _ => ZoomEnd();
+	}
+	private void ZoomStart() {
+		zoomCoroutine = StartCoroutine(ZoomDetection());
+	}
+	private void ZoomEnd() {
+		StopCoroutine(zoomCoroutine);
+	}
+	IEnumerator ZoomDetection() {
+		float previousDistance = 0f, distance = 0f;
+		while(true) {
+			distance = Vector2.Distance(controls.Touch.PrimaryFingerPosition.ReadValue<Vector2>(), controls.Touch.SecondaryFingerPosition.ReadValue<Vector2>());
+			// Detection
+			// Zoom Out
+			if(previousDistance > 0f) {
+				// Factor < 0 when we are zooming in. (Fingers get closer to each other)
+				// Factor > 0 when are zooming out. (Fingers go far from each other)
+				float factor = distance / previousDistance;
+				Vector3 oldScale = go.transform.localScale;
+				// Short for: factor + ((factor - 1f) * (speed - 1f))
+				Vector3 newScale = oldScale * (factor * speed - speed + 1);
+				float newScaleMagnitude = newScale.magnitude;
+				if(newScaleMagnitude < minScale) {
+					go.transform.localScale = newScale.normalized * minScale;
+				} else if(newScaleMagnitude > maxScale) {
+					go.transform.localScale = Vector3.ClampMagnitude(newScale, maxScale);
+				} else if(newScaleMagnitude > minScale && newScaleMagnitude < maxScale) {
+					go.transform.localScale = newScale;
+				}
+			}
+			previousDistance = distance;
+			yield return null;
+		}
+	}
+}
